e1016d687232bde0540b62bd719e5843362d69e9
Cheakimse Mao <cheakimsemao@Cheakimses-MacBook-Pro.local>
2020-09-12 22:08:27 +0700

    Added Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <p>Header</p>
+        <div id='header' className='flex-container'>
+            <div className='center brand'>
+                <h1 className='header-title'>TODO-APP</h1>
+                <hr className='header-line' />
+                <p className='header-datetime'>9 Sep 2020, Wednesday</p>
+            </div>
+            <div className='progress'>
+                <div>
+                    <p className='progress-title'>TOTAL</p>
+                    <p className='progress-number'>18</p>
+                </div>
+                <div>
+                    <p className='progress-title'>COMPLETED</p>
+                    <p className='progress-number'>9</p>
+                </div>
+                <div>
+                    <p className='progress-title'>DONE</p>
+                    <p className='progress-number'>50%</p>
+                </div>
+            </div>
         </div>
     );
 };
