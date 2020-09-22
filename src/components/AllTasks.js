8b46cf04af3f5dc4603b4adb22d392644d2ed529
Cheakimse Mao <cheakimsemao@Cheakimses-MacBook-Pro.local>
2020-09-22 22:23:37 +0700

    Added All Tasks & Completed Tasks UI

--- src/components/AllTasks.js
+++ src/components/AllTasks.js
@@ -1,9 +1,80 @@
 import React from 'react';
+import { MdEdit, MdDelete } from 'react-icons/md';
 
 const AllTasks = () => {
     return (
-        <div>
-            <p>AllTasks</p>
+        <div id='all-tasks'>
+            <div className='flex-container'>
+                <div>
+                    <p className='allTasks-title'>All Tasks</p>
+                </div>
+                <button className='add-btn'>ADD</button>
+            </div>
+            <hr className='task-line' />
+            <div className='flex-container'>
+                <div className='todo-item'>
+                    <input type='checkbox' />
+                    <span>Meeting With Friends</span>
+                </div>
+                <div>
+                    <a type='button'>
+                        <MdEdit className='edit-icon' />
+                    </a>
+                    <a type='button'>
+                        <MdDelete className='delete-icon' />
+                    </a>
+                </div>
+            </div>
+            <small className='deadline'>Today, 9:00 PM</small>
+            <hr className='task-line' />
+            <div className='flex-container'>
+                <div className='todo-item'>
+                    <input type='checkbox' />
+                    <span>Meeting With Friends</span>
+                </div>
+                <div>
+                    <a type='button'>
+                        <MdEdit className='edit-icon' />
+                    </a>
+                    <a type='button'>
+                        <MdDelete className='delete-icon' />
+                    </a>
+                </div>
+            </div>
+            <small className='deadline'>Today, 9:00 PM</small>
+            <hr className='task-line' />
+            <div className='flex-container'>
+                <div className='todo-item'>
+                    <input type='checkbox' />
+                    <span>Meeting With Friends</span>
+                </div>
+                <div>
+                    <a type='button'>
+                        <MdEdit className='edit-icon' />
+                    </a>
+                    <a type='button'>
+                        <MdDelete className='delete-icon' />
+                    </a>
+                </div>
+            </div>
+            <small className='deadline'>Today, 9:00 PM</small>
+            <hr className='task-line' />
+            <div className='flex-container'>
+                <div className='todo-item'>
+                    <input type='checkbox' />
+                    <span>Meeting With Friends</span>
+                </div>
+                <div>
+                    <a type='button'>
+                        <MdEdit className='edit-icon' />
+                    </a>
+                    <a type='button'>
+                        <MdDelete className='delete-icon' />
+                    </a>
+                </div>
+            </div>
+            <small className='deadline'>Today, 9:00 PM</small>
+            <hr className='task-line' />
         </div>
     );
 };
--- src/components/CompletedTasks.js
+++ src/components/CompletedTasks.js
@@ -2,8 +2,42 @@ import React from 'react';
 
 const CompletedTasks = () => {
     return (
-        <div>
-            <p>CompletedTasks</p>
+        <div id='completed-tasks'>
+            <div className='flex-container'>
+                <p className='completedTasks-title'>Completed</p>
+                <button className='add-btn'>HIDE</button>
+            </div>
+            <hr className='task-line' />
+            <div className='flex-container'>
+                <div className='todo-item'>
+                    <input type='checkbox' checked />
+                    <span>
+                        <s>Meeting With Friends</s>
+                    </span>
+                </div>
+                <div>
+                    <a type='button' className='undo-task'>
+                        UNDO
+                    </a>
+                </div>
+            </div>
+            <small className='deadline'>Today, 9:00 PM</small>
+            <hr className='task-line' />
+            <div className='flex-container'>
+                <div className='todo-item'>
+                    <input type='checkbox' checked />
+                    <span>
+                        <s>Meeting With Friends</s>
+                    </span>
+                </div>
+                <div>
+                    <a type='button' className='undo-task'>
+                        UNDO
+                    </a>
+                </div>
+            </div>
+            <small className='deadline'>Today, 9:00 PM</small>
+            <hr className='task-line' />
         </div>
     );
 };
